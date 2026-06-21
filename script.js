f71f97c6725dd6a7e9cdc8575d0d97c7a17f0bd2
JohnVilla <129530641+johnathanvillalona@users.noreply.github.com>
2026-06-21 16:38:08 -0400

    Add global variables to keep track of score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
     //Assign randomNumber an int between 1 and 3, inclusive
     let randomNumber = Math.floor(Math.random()*3)+1;
