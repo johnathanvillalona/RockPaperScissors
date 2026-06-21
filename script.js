cd5f7a1a67b6314446265e123d069d51a73fd572
JohnVilla <129530641+johnathanvillalona@users.noreply.github.com>
2026-06-21 16:29:14 -0400

    Create a function to return rock, paper, or scissors
    Created a function which gets a random number between 1 and 3, then assigns a corresponding string of rock, paper, or scissors; and finally returns the result

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function getComputerChoice() {
     //Assign randomNumber an int between 1 and 3, inclusive
     let randomNumber = Math.floor(Math.random()*3)+1;
+    let randomChoice;
+    console.log(randomNumber)
 
+    //Assign rock, paper, or scissors as strings to each number between 1 and 3, inclusive
+    switch (randomNumber) {
+        case 1:
+            randomChoice = "Rock";
+            break;
+        case 2:
+            randomChoice = "Scissors";
+            break;
+        case 3:
+            randomChoice = "Paper";
+            break;
+    }
+    return randomChoice;
 }
+
+//for testing function output
+console.log(getComputerChoice());
+
+
